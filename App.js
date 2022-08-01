adde19983929e3945ba58fca744fdca5e8269ace
Tony Correia <luis.dhac@gmail.com>
2022-08-01 15:59:20 -0300

    Styling based on Platform

--- App.js
+++ App.js
@@ -1,35 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor:'silver',
-       padding: 50,
-        flex: 1,
-         flexDirection: "row",
-         justifyContent: "space-around",
-         alignItems: "flex-start"
-         }}>
-      <View style={{width: 60, height:60, backgroundColor:'red'}}>
-        <Text>A</Text>
-      </View>
-      <View style={{width: 60, height:60, backgroundColor:'green'}}>
-        <Text>B</Text>
-      </View>
-      <View style={{width: 60, height:60, backgroundColor:'blue'}}>
-        <Text>C</Text>
-      </View>
-
-    </View>
+          <View>
+            <Header/>
+          </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
