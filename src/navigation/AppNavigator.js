de3a5c80537f02de6489196bed415f59181d7349
Tony Correia <luis.dhac@gmail.com>
2022-08-02 17:36:22 -0300

    Adding a tabs-bar Navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,27 +1,61 @@
 import React from 'react'
 import  {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import NewsListScreen from '../screens/NewsListScreen'
 import NewsDetailsScreen from '../screens/NewsDetailsScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
+import MaterialIcons from '@expo/vector-icons'
 
 const Stack = createNativeStackNavigator()
+
+const Tabs = createBottomTabNavigator()
+
+function HomeNavigator(){
+    return(
+        <Stack.Navigator>
+        <Stack.Screen 
+        name="NewsList" 
+        component={NewsListScreen} 
+        options={{title: 'All News'}}
+        />
+        <Stack.Screen 
+        name="NewsDetails" 
+        component={NewsDetailsScreen} 
+        options={{title: 'News Details'}}
+        />
+    </Stack.Navigator>
+    )
+}
+
+function FavoriteNavigator(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Favorites" component={FavoritesScreen} />
+        </Stack.Navigator>
+    )
+}
 
 function AppNavigator(){
     return(
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen 
-                name="NewsList" 
-                component={NewsListScreen} 
-                options={{title: 'All News'}}
-                />
-                <Stack.Screen 
-                name="NewsDetails" 
-                component={NewsDetailsScreen} 
-                options={{title: 'News Details'}}
-                />
-            </Stack.Navigator>
+           <Tabs.Navigator 
+                       screenOptions={({route})=> ({
+                        tabBarIcon: () => {
+                            let iconName;
+                            if(route.name=="Home"){
+                                iconName="home"
+                            } else if (route.name=="Favorite") {
+                                iconName="favorite"
+                            }
+                            return <MaterialIcons name={iconName} size={24} />
+                        }
+                       })}
+           >
+               <Tabs.Screen  name="Home" component={HomeNavigator} />
+               <Tabs.Screen name="Favorite" component={FavoriteNavigator}/>
+           </Tabs.Navigator>
         </NavigationContainer>
     )
 }
